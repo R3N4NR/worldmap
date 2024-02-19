50a878a8259a03ec0e5aa95feb644761e00df35e
R3N4NR <renanramalhou@gmail.com>
2024-02-18 21:36:27 -0300

    sobe o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import { ReactComponent as World } from './world.svg';
 import './App.css';
+import { HexColorPicker } from 'react-colorful';
 
 function App() {
+  const svgRef = useRef(null);
+  const [color, setColor] = useState("#aabbcc");
+  const [country, setCountry] = useState<string>('')
+
+ 
+  const handleClick = (event: any) => {
+    
+   
+    const target = event.target;
+
+    const getName = target.getAttribute('name')
+    setCountry(getName)
+
+
+    if (target instanceof SVGElement && target.tagName === 'path') {
+
+      const path = target;
+
+      path.setAttribute('fill', color);
+    }
+
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="map-container">
+        <World style={{ maxWidth: '100%', maxHeight: '100vh' }} ref={svgRef} onClick={handleClick} />
+        <div className="colorPicker" style={{ position: 'absolute', bottom: '10px', left: '10px' }}>
+          <HexColorPicker color={color} onChange={setColor} />
+        </div>
+        <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
+          <span>Country : {country}</span>
+        </div>
+      </div>
     </div>
   );
 }
